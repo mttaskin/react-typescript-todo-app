873aa983832752af84811ff25d97838994c325ab
mttaskin <0mttaskin1@gmail.com>
2023-04-08 06:47:09 +0300

    v1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,13 @@ import React, { useEffect, useState } from 'react'
 import InputForm from '../components/InputForm'
 import TodoList from '../components/TodoList';
 
+const url = "https://6402fe07f61d96ac4874ce6b.mockapi.io/todos"
 
-const Home = () => {
+interface Todotype {
   
+}
+const Home = () => {
+  const [todos, setTodos] = useState([])
   
 
   return (
