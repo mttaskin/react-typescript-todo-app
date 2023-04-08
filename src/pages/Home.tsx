327dca2038e4a1796a2a009b294ba868e19cbb41
mttaskin <0mttaskin1@gmail.com>
2023-04-08 07:00:59 +0300

    v2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,37 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import InputForm from '../components/InputForm'
 import TodoList from '../components/TodoList';
 
 const url = "https://6402fe07f61d96ac4874ce6b.mockapi.io/todos"
 
 interface Todotype {
-  
+id:string | number;
+task:string;
+isDone:boolean;
 }
+
 const Home = () => {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState<Todotype[]>([])
   
+const getTodos = async () =>{
+  try {
+    const {data} = await axios.get<Todotype[]>(url)
+    setTodos(data)
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+useEffect(() => {
+  getTodos()
+  
+}, [])
 
   return (
     <div className='main'>
       <InputForm  />
-      <TodoList />
+      <TodoList todos={todos}/>
     </div>
   )
 }
